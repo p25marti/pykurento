ab4a661f39d1e0165dd4b0d0dca3f3cb6562d691
p25marti <paulmartins4242@gmail.com>
2019-10-24 16:23:54 -0400

    pep8 things

--- examples/app.py
+++ examples/app.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import examples.multires.handlers
+import examples.rooms.handlers
+import examples.loopback.handlers
+from examples import render_view
 import os
 import sys
 import logging
@@ -11,30 +15,28 @@ import tornado.web
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 logging.getLogger().setLevel(logging.DEBUG)
 
-from examples import render_view
-import examples.loopback.handlers
-import examples.rooms.handlers
-import examples.multires.handlers
-
 
 class IndexHandler(tornado.web.RequestHandler):
-  def get(self):
-    render_view(self, "index")
+    def get(self):
+        render_view(self, "index")
+
 
 application = tornado.web.Application([
-  (r"/", IndexHandler),
-  (r"/loopback", examples.loopback.handlers.LoopbackHandler),
-  (r"/multires", examples.multires.handlers.MultiResHandler),
-  (r"/room", examples.rooms.handlers.RoomIndexHandler),
-  (r"/room/(?P<room_id>\d*)", examples.rooms.handlers.RoomHandler),
-  (r"/room/(?P<room_id>[^/]*)/subscribe/(?P<from_participant_id>[^/]*)/(?P<to_participant_id>[^/]*)", examples.rooms.handlers.SubscribeToParticipantHandler),
-  (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': os.path.join(os.path.dirname(__file__), "static")}),
+    (r"/", IndexHandler),
+    (r"/loopback", examples.loopback.handlers.LoopbackHandler),
+    (r"/multires", examples.multires.handlers.MultiResHandler),
+    (r"/room", examples.rooms.handlers.RoomIndexHandler),
+    (r"/room/(?P<room_id>\d*)", examples.rooms.handlers.RoomHandler),
+    (r"/room/(?P<room_id>[^/]*)/subscribe/(?P<from_participant_id>[^/]*)/(?P<to_participant_id>[^/]*)",
+        examples.rooms.handlers.SubscribeToParticipantHandler),
+    (r'/static/(.*)', tornado.web.StaticFileHandler,
+        {'path': os.path.join(os.path.dirname(__file__), "static")}),
 ], debug=True)
 
 if __name__ == "__main__":
-  port = int(os.environ.get("PORT", 8080))
-  application.listen(port)
-  print("Webserver now listening on port %d" % port)
-  ioloop = tornado.ioloop.IOLoop.instance()
-  signal.signal(signal.SIGINT, lambda sig, frame: ioloop.stop())
-  ioloop.start()
+    port = int(os.environ.get("PORT", 8080))
+    application.listen(port)
+    print("Webserver now listening on port %d" % port)
+    ioloop = tornado.ioloop.IOLoop.instance()
+    signal.signal(signal.SIGINT, lambda sig, frame: ioloop.stop())
+    ioloop.start()
--- examples/rooms/models.py
+++ examples/rooms/models.py
@@ -12,56 +12,59 @@ Base = declarative_base(bind=engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+
 def get_session():
-  return session
+    return session
+
 
 class Participant(Base):
-  __tablename__ = "participants"
+    __tablename__ = "participants"
 
-  id = Column(Integer, primary_key=True)
-  incoming_endpoint_id = Column(String)
-  room_id = Column(Integer, ForeignKey('rooms.id'))
+    id = Column(Integer, primary_key=True)
+    incoming_endpoint_id = Column(String)
+    room_id = Column(Integer, ForeignKey('rooms.id'))
 
-  room = relationship("Room", backref=backref('participants', order_by=id))
+    room = relationship("Room", backref=backref('participants', order_by=id))
 
-  def get_incoming(self):
-    if self.incoming_endpoint_id:
-      incoming = media.WebRtcEndpoint(self.room.get_pipeline(), id=self.incoming_endpoint_id)
-    else:
-      incoming = media.WebRtcEndpoint(self.room.get_pipeline())
-      self.incoming_endpoint_id = incoming.id
-      session = get_session()
-      session.add(self)
-      session.commit()
-    return incoming
+    def get_incoming(self):
+        if self.incoming_endpoint_id:
+            incoming = media.WebRtcEndpoint(
+                self.room.get_pipeline(), id=self.incoming_endpoint_id)
+        else:
+            incoming = media.WebRtcEndpoint(self.room.get_pipeline())
+            self.incoming_endpoint_id = incoming.id
+            session = get_session()
+            session.add(self)
+            session.commit()
+        return incoming
 
-  def get_answer(self, offer):
-    return self.get_incoming().process_offer(offer)
+    def get_answer(self, offer):
+        return self.get_incoming().process_offer(offer)
 
-  def connect(self, offer):
-    incoming = self.get_incoming()
-    outgoing = media.WebRtcEndpoint(self.room.get_pipeline())
-    incoming.connect(outgoing)
-    return outgoing.process_offer(offer)
+    def connect(self, offer):
+        incoming = self.get_incoming()
+        outgoing = media.WebRtcEndpoint(self.room.get_pipeline())
+        incoming.connect(outgoing)
+        return outgoing.process_offer(offer)
 
 
 class Room(Base):
-  __tablename__ = "rooms"
+    __tablename__ = "rooms"
 
-  id = Column(Integer, primary_key=True)
-  pipeline_id = Column(String)
+    id = Column(Integer, primary_key=True)
+    pipeline_id = Column(String)
 
-  def get_pipeline(self):
-    if self.pipeline_id:
-      pipeline = kurento.get_pipeline(self.pipeline_id)
-    else:
-      pipeline = kurento.create_pipeline()
-      self.pipeline_id = pipeline.id
-      session = get_session()
-      session.add(self)
-      session.commit()
+    def get_pipeline(self):
+        if self.pipeline_id:
+            pipeline = kurento.get_pipeline(self.pipeline_id)
+        else:
+            pipeline = kurento.create_pipeline()
+            self.pipeline_id = pipeline.id
+            session = get_session()
+            session.add(self)
+            session.commit()
 
-    return pipeline
+        return pipeline
 
 
 Base.metadata.create_all(engine)
